87c3a00659e34494c781e7f736e73d653c2ee2be
Jaydeep <75525428+jaydeep-thakor@users.noreply.github.com>
2026-04-06 12:28:38 +0530

    Fix comments for variable types in 2-Variables.java
    Updated comments for clarity and consistency in variable declarations.

--- 1-basic/2-Variables.java
+++ 1-basic/2-Variables.java
@@ -24,18 +24,20 @@ class Variables {
         // ===== Primitive types =====
         
         // Integer types: byte, short, int, long
-        int n = 9;             // 4 byte      // 32 bits        min = -2^(32-1)        max = 2^(32-1)-1
-        byte b = 127;          // 1 bytes     // 8 bits         min = -2^(8-1)         max = 2^(8-1)-1          
+        int n = 9;             // 4 bytes      // 32 bits        min = -2^(32-1)        max = 2^(32-1)-1
+        byte b = 127;          // 1 byte     // 8 bits         min = -2^(8-1)         max = 2^(8-1)-1          
         short s = 400;         // 2 bytes     // 16 bits        min = -2^(16-1)        max = 2^(16-1)-1 
-        long l = 7898l;        // 8 bytes     // 64 bits        min = -2^(64-1)        max = 2^(64-1)-1
+        long l = 7898L;        // 8 bytes     // 64 bits        min = -2^(64-1)        max = 2^(64-1)-1
         // Floating-point types: float, double
         double d = 20.10;      // 8 bytes     // 64 bits        min = -1.7×10^308      max = 1.7×10^308
-        float f = 5.6f;        // 4 bytes     // 32 bits        min = -3.4×10^38       max = 3.4×10^38
+        float f = 5.6F;        // 4 bytes     // 32 bits        min = -3.4×10^38       max = 3.4×10^38
         // Character type: char
         char c = 'j';          // 2 bytes     // 16 bits        min = 0                max = 65,535  
         // Boolean type: boolean
         boolean bl = true;     // 1 byte      // 8 bits         only → true or false            
 
+        // we can use capital l or f for long and float both will work same
+        
         // ===== Non-primitive types ===== size depends on how many characters
 
         // String (most commonly used)
