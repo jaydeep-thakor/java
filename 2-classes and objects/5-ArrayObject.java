f9b39239486fa1850def71ea86f78ce2cecf53e8
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-04-12 16:30:47 +0530

    add code about constructor

--- 2-classes and objects/5-ArrayObject.java
+++ 2-classes and objects/5-ArrayObject.java
@@ -21,8 +21,13 @@ class ArrayObject{
         students[0] = s1;
         students[1] = s2;
 
-        for(int i = 0; i<students.length; i++){
-            System.out.println(students[i].rollNo + " " + students[i].name + " " + students[i].marks);
+        // for(int i = 0; i<students.length; i++){
+        //     System.out.println(students[i].rollNo + " " + students[i].name + " " + students[i].marks);
+        // }
+
+        // enhanced for loop
+        for(Student stud : students){
+            System.out.println(stud.rollNo + " " + stud.name + " " stud.marks);
         }
 
     }
